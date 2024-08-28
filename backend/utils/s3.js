b4c140eb19fdd2ecc32892fc493780dfc413dd6e
SlayerDevv <hdfhoussam18@gmail.com>
2024-08-29 00:36:35 +0100

    e

--- backend/utils/s3.js
+++ backend/utils/s3.js
@@ -2,7 +2,7 @@ const {S3Client, ListBucketsCommand} = require('@aws-sdk/client-s3')
 require('dotenv').config()
  const client = new S3Client({region: 'auto', credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEy,
 }, endpoint: process.env.AWS_API, })
 
 module.exports = {client}
